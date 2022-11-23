aa8592857e34c9f0542447a449dd24ec9e389951
Rafael Alejandro Iriarte Baez <20030577@uadeo.mx>
2022-11-23 11:11:29 -0700

    frmpedido con datatimepicker

--- forms/frmpedidos.Designer.cs
+++ forms/frmpedidos.Designer.cs
@@ -28,50 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpfecha = new System.Windows.Forms.DateTimePicker();
             this.btnsalir = new System.Windows.Forms.Button();
             this.btneliminar = new System.Windows.Forms.Button();
             this.btnactualizar = new System.Windows.Forms.Button();
             this.btninsertar = new System.Windows.Forms.Button();
             this.dgvarticulos = new System.Windows.Forms.DataGridView();
-            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtclienteid = new System.Windows.Forms.TextBox();
             this.txtid = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtdescripcion = new WindowsFormsApp2.Utilerias.Validacion();
+            this.txtstatus = new WindowsFormsApp2.Utilerias.Validacion();
+            this.txtEmpleado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvarticulos)).BeginInit();
             this.SuspendLayout();
             // 
-            // dateTimePicker1
+            // dtpfecha
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(125, 100);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(271, 30);
-            this.dateTimePicker1.TabIndex = 38;
-            this.dateTimePicker1.UseWaitCursor = true;
+            this.dtpfecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpfecha.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpfecha.Location = new System.Drawing.Point(94, 81);
+            this.dtpfecha.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpfecha.Name = "dtpfecha";
+            this.dtpfecha.Size = new System.Drawing.Size(204, 26);
+            this.dtpfecha.TabIndex = 38;
+            this.dtpfecha.UseWaitCursor = true;
             // 
             // btnsalir
             // 
             this.btnsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnsalir.Location = new System.Drawing.Point(611, 86);
-            this.btnsalir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnsalir.Location = new System.Drawing.Point(458, 70);
+            this.btnsalir.Margin = new System.Windows.Forms.Padding(2);
             this.btnsalir.Name = "btnsalir";
-            this.btnsalir.Size = new System.Drawing.Size(177, 36);
+            this.btnsalir.Size = new System.Drawing.Size(133, 29);
             this.btnsalir.TabIndex = 37;
-            this.btnsalir.Text = "Mostrar";
+            this.btnsalir.Text = "Salir";
             this.btnsalir.UseVisualStyleBackColor = true;
             // 
             // btneliminar
             // 
             this.btneliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btneliminar.Location = new System.Drawing.Point(611, 21);
-            this.btneliminar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btneliminar.Location = new System.Drawing.Point(458, 17);
+            this.btneliminar.Margin = new System.Windows.Forms.Padding(2);
             this.btneliminar.Name = "btneliminar";
-            this.btneliminar.Size = new System.Drawing.Size(177, 36);
+            this.btneliminar.Size = new System.Drawing.Size(133, 29);
             this.btneliminar.TabIndex = 36;
             this.btneliminar.Text = "Deshabilitar";
             this.btneliminar.UseVisualStyleBackColor = true;
@@ -79,10 +81,10 @@
             // btnactualizar
             // 
             this.btnactualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnactualizar.Location = new System.Drawing.Point(419, 86);
-            this.btnactualizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnactualizar.Location = new System.Drawing.Point(314, 70);
+            this.btnactualizar.Margin = new System.Windows.Forms.Padding(2);
             this.btnactualizar.Name = "btnactualizar";
-            this.btnactualizar.Size = new System.Drawing.Size(177, 36);
+            this.btnactualizar.Size = new System.Drawing.Size(133, 29);
             this.btnactualizar.TabIndex = 35;
             this.btnactualizar.Text = "Actualizar";
             this.btnactualizar.UseVisualStyleBackColor = true;
@@ -90,10 +92,10 @@
             // btninsertar
             // 
             this.btninsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btninsertar.Location = new System.Drawing.Point(419, 21);
-            this.btninsertar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btninsertar.Location = new System.Drawing.Point(314, 17);
+            this.btninsertar.Margin = new System.Windows.Forms.Padding(2);
             this.btninsertar.Name = "btninsertar";
-            this.btninsertar.Size = new System.Drawing.Size(177, 36);
+            this.btninsertar.Size = new System.Drawing.Size(133, 29);
             this.btninsertar.TabIndex = 33;
             this.btninsertar.Text = "Guardar";
             this.btninsertar.UseVisualStyleBackColor = true;
@@ -101,37 +103,38 @@
             // dgvarticulos
             // 
             this.dgvarticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvarticulos.Location = new System.Drawing.Point(52, 145);
-            this.dgvarticulos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvarticulos.Location = new System.Drawing.Point(29, 165);
+            this.dgvarticulos.Margin = new System.Windows.Forms.Padding(2);
             this.dgvarticulos.Name = "dgvarticulos";
             this.dgvarticulos.ReadOnly = true;
             this.dgvarticulos.RowTemplate.Height = 24;
-            this.dgvarticulos.Size = new System.Drawing.Size(715, 288);
+            this.dgvarticulos.Size = new System.Drawing.Size(536, 234);
             this.dgvarticulos.TabIndex = 34;
             // 
-            // txtnombre
+            // txtclienteid
             // 
-            this.txtnombre.Location = new System.Drawing.Point(125, 63);
-            this.txtnombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtnombre.Name = "txtnombre";
-            this.txtnombre.Size = new System.Drawing.Size(271, 22);
-            this.txtnombre.TabIndex = 30;
+            this.txtclienteid.Location = new System.Drawing.Point(94, 51);
+            this.txtclienteid.Margin = new System.Windows.Forms.Padding(2);
+            this.txtclienteid.Name = "txtclienteid";
+            this.txtclienteid.Size = new System.Drawing.Size(204, 20);
+            this.txtclienteid.TabIndex = 30;
             // 
             // txtid
             // 
-            this.txtid.Location = new System.Drawing.Point(125, 21);
-            this.txtid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtid.Location = new System.Drawing.Point(94, 17);
+            this.txtid.Margin = new System.Windows.Forms.Padding(2);
             this.txtid.Name = "txtid";
-            this.txtid.Size = new System.Drawing.Size(83, 22);
+            this.txtid.Size = new System.Drawing.Size(63, 20);
             this.txtid.TabIndex = 27;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(215, 17);
+            this.label4.Location = new System.Drawing.Point(161, 14);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 25);
+            this.label4.Size = new System.Drawing.Size(76, 20);
             this.label4.TabIndex = 31;
             this.label4.Text = "Estatus:";
             // 
@@ -139,9 +142,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(33, 59);
+            this.label2.Location = new System.Drawing.Point(25, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.Size = new System.Drawing.Size(70, 20);
             this.label2.TabIndex = 29;
             this.label2.Text = "Cliente:";
             // 
@@ -149,43 +153,64 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(80, 20);
+            this.label1.Location = new System.Drawing.Point(60, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 25);
+            this.label1.Size = new System.Drawing.Size(33, 20);
             this.label1.TabIndex = 28;
             this.label1.Text = "ID:";
             // 
-            // txtdescripcion
+            // txtstatus
             // 
-            this.txtdescripcion.Location = new System.Drawing.Point(323, 21);
-            this.txtdescripcion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtdescripcion.Name = "txtdescripcion";
-            this.txtdescripcion.Size = new System.Drawing.Size(73, 22);
-            this.txtdescripcion.SoloLetras = false;
-            this.txtdescripcion.SoloNumeros = false;
-            this.txtdescripcion.TabIndex = 39;
-            this.txtdescripcion.Validar = true;
-            this.txtdescripcion.VRFC = false;
-            this.txtdescripcion.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtdescripcion_KeyPress);
+            this.txtstatus.Location = new System.Drawing.Point(242, 17);
+            this.txtstatus.Name = "txtstatus";
+            this.txtstatus.Size = new System.Drawing.Size(56, 20);
+            this.txtstatus.SoloLetras = false;
+            this.txtstatus.SoloNumeros = false;
+            this.txtstatus.TabIndex = 39;
+            this.txtstatus.Validar = true;
+            this.txtstatus.VRFC = false;
+            this.txtstatus.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtdescripcion_KeyPress);
+            // 
+            // txtEmpleado
+            // 
+            this.txtEmpleado.Location = new System.Drawing.Point(123, 123);
+            this.txtEmpleado.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmpleado.Name = "txtEmpleado";
+            this.txtEmpleado.Size = new System.Drawing.Size(175, 20);
+            this.txtEmpleado.TabIndex = 42;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 120);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Empleado:";
             // 
             // frmpedidos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtdescripcion);
-            this.Controls.Add(this.dateTimePicker1);
+            this.ClientSize = new System.Drawing.Size(600, 410);
+            this.Controls.Add(this.txtEmpleado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtstatus);
+            this.Controls.Add(this.dtpfecha);
             this.Controls.Add(this.btnsalir);
             this.Controls.Add(this.btneliminar);
             this.Controls.Add(this.btnactualizar);
             this.Controls.Add(this.btninsertar);
             this.Controls.Add(this.dgvarticulos);
-            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txtclienteid);
             this.Controls.Add(this.txtid);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmpedidos";
             this.Text = "frmpedidos";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -197,18 +222,20 @@
 
         #endregion
 
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpfecha;
         private System.Windows.Forms.Button btnsalir;
         private System.Windows.Forms.Button btneliminar;
         private System.Windows.Forms.Button btnactualizar;
         private System.Windows.Forms.Button btninsertar;
         private System.Windows.Forms.DataGridView dgvarticulos;
-        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtclienteid;
         private System.Windows.Forms.TextBox txtid;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private Utilerias.Validacion txtdescripcion;
+        private Utilerias.Validacion txtstatus;
+        private System.Windows.Forms.TextBox txtEmpleado;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- forms/frmpedidos.cs
+++ forms/frmpedidos.cs
@@ -7,22 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using WindowsFormsApp2.clases;
+using System.Globalization;
+
 
 namespace WindowsFormsApp2
 {
     public partial class frmpedidos : Form
     {
+        clases.Conexion objconection;
+        SqlConnection conexion;
+        string sConexion;
         public frmpedidos()
         {
             InitializeComponent();
+            sConexion = Conexion.conn();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            dateTimePicker1.Format = DateTimePickerFormat.Custom;
-            dateTimePicker1.CustomFormat = "MM/dd/yyyy hh:mm:ss";
+            this.pEDIDOTableAdapter2.Fill(this.panesitoDataSet5.PEDIDO);
+            this.pEDIDO_DETALLETableAdapter1.Fill(this.panesitoDataSetPed.PEDIDO_DETALLE);
+            this.pEDIDOTableAdapter1.Fill(this.panesitoDataSetPed.PEDIDO);
+
+            //suppan
+            dtpfecha.Format = DateTimePickerFormat.Custom;
+            dtpfecha.CustomFormat = "MM/dd/yyyy hh:mm:ss";
+            consecutivo();
         }
 
+        private void GuardarPedidos()
+        {
+
+            clspedido pedido = new clspedido();
+            pedido.iId_cliente = Convert.ToInt32(txtid.Text);
+            pedido.iId_empleado = Convert.ToInt32(txtid.Text);
+            pedido.d_fecha = dtpfecha.Value;
+
+
+
+
+            pedido.bStatus_pedido = txtstatus.Text;
+            if (pedido.Guardar() == true)
+            {
+                MessageBox.Show("Datos Guardados Correctamente");
+                limpiar();
+            }
+            else
+            {
+                MessageBox.Show("Error al guardar los datos" + pedido.error);
+            }
+        }
+
+        private void consecutivo()
+        {
+            SqlConnection conn = new SqlConnection(sConexion);
+            SqlCommand cmd = new SqlCommand("", conn);
+            SqlDataReader l;
+
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT ISNULL(MAX(pe_id_pedido),0) + 1 AS consecutivo FROM PEDIDO";
+
+            conn.Open();
+            l = cmd.ExecuteReader();
+            if (l.Read())
+            {
+                txtid.Text = Convert.ToString(l.GetInt32(0));
+            }
+
+        }
+        private void limpiar()
+        {
+            txtid.Clear();
+            txtid.Clear();
+            txtEmpleado.Clear();
+            consecutivo();
+            this.pEDIDOTableAdapter2.Fill(this.panesitoDataSet5.PEDIDO);
+        }
+        private void btninsertar_Click(object sender, EventArgs e)
+        {
+            GuardarPedidos();
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+        private void dgvarticulos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtid.Text = this.panesitoDataSet5.PEDIDO[pEDIDOBindingSource2.Position].pe_id_pedido.ToString();
+            txtid.Text = this.panesitoDataSet5.PEDIDO[pEDIDOBindingSource2.Position].pe_id_cliente.ToString();
+            txtEmpleado.Text = this.panesitoDataSet5.PEDIDO[pEDIDOBindingSource2.Position].pe_id_empleado.ToString();
+            dtpfecha.Text = this.panesitoDataSet5.PEDIDO[pEDIDOBindingSource2.Position].pe_fecha.ToString();
+            txtstatus.Text = this.panesitoDataSet5.PEDIDO[pEDIDOBindingSource2.Position].pe_estatus_pedido.ToString();
+        }
         private void txtdescripcion_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((e.KeyChar >= 33 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
@@ -30,9 +109,11 @@ namespace WindowsFormsApp2
 
                 //errorNombre.SetError(txtnombre, "Ingrese solo Letras");
                 MessageBox.Show("Ingrese Solo Letras");
-                txtdescripcion.Clear();
-                txtdescripcion.Focus();
+                txtstatus.Clear();
+                txtstatus.Focus();
             }
         }
+
+
     }
 }
